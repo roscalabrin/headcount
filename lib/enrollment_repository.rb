c3d8cb3a861e399a71b68a8432d3c9e518516aa1
tgisg <soniagupta504@gmail.com>
2016-06-07 20:15:00 -0600

    working on er creating enrollment object

--- lib/enrollment_repository.rb
+++ lib/enrollment_repository.rb
@@ -1,25 +1,50 @@
 require 'csv'
 require 'pry'
 require_relative 'enrollment'
+
 class EnrollmentRepository
+  attr_reader :enrollment_collection
+
+  def initialize
+    @enrollment_collection = {}
+  end
 
   def load_data(file_tree)
     filepath = file_tree[:enrollment][:kindergarten]
-    years = []
     CSV.foreach(filepath, headers: true, header_converters: :symbol) do |row|
-      years << ({:name => row[:location], row[:timeframe].to_i => row[:data].to_f})
+      district_name = row[:location]
+      year = row[:timeframe]
+      data = row[:data]
+      enrollment_object = Enrollment.new({:name => district_name, :kindergarten_participation => {year => data}})
+
+      #refactor for above code:
+      # name = row[:location]
+      # district_collection[name] = District.new({:name => name}
     end
-    puts years
+    binding.pry
   end
 
   def find_by_name(district_name)
-    return district_name.upcase if 
+    "test"
 
   end
+
 end
+
+#	#need to take any CSV, pull only enrollement information out of it, then attach that district name to instance of district class (object) and then stick that object into @district_collection
+
 
 # e = EnrollmentRepository.new
 # puts e.load_data({
 #   :enrollment => {
 #     :kindergarten => "./data/Kindergartners in full-day program.csv"
 #   }})
+
+# def load_data(file_tree)
+#   filepath = file_tree[:enrollment][:kindergarten]
+#   years = []
+#   CSV.foreach(filepath, headers: true, header_converters: :symbol) do |row|
+#     years << ({:name => row[:location], row[:timeframe].to_i => row[:data].to_f})
+#   end
+#   puts years
+# end
--- test/enrollment_repository_test.rb
+++ test/enrollment_repository_test.rb
@@ -4,13 +4,20 @@ require './lib/enrollment'
 
 class EnrollmentRepositoryTest < Minitest::Test
 
-  def test_it_finds_instance_of_enrollment_by_name_if_exist
+  def test_that_it_loads_data_file
     er = EnrollmentRepository.new
     er.load_data({:enrollment => {:kindergarten => "./data/Kindergartners in full-day program.csv"}})
-    enrollment = er.find_by_name("ACADEMY 20")
-    # enrollment = Enrollment.new({:name => "ACADEMY 20", :kindergarten_participation => {2010 => 0.3915, 2011 => 0.35356, 2012 => 0.2677}})
 
-
-    assert_instance_of Enrollment, enrollment
+    assert_equal 181, er.enrollment_collection.length
   end
+
+  # def test_it_finds_instance_of_enrollment_by_name_if_exist
+  #   er = EnrollmentRepository.new
+  #   er.load_data({:enrollment => {:kindergarten => "./data/Kindergartners in full-day program.csv"}})
+  #   enrollment = er.find_by_name("ACADEMY 20")
+  #   # enrollment = Enrollment.new({:name => "ACADEMY 20", :kindergarten_participation => {2010 => 0.3915, 2011 => 0.35356, 2012 => 0.2677}})
+  #
+  #
+  #   assert_instance_of Enrollment, enrollment
+  # end
 end
