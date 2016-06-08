6964c421f56dfea44bc123bf7ef9ad4db8d31298
Roberta Doyle <ro.scalabrin@gmail.com>
2016-06-07 19:38:07 -0600

    Finished district repository

--- lib/district.rb
+++ lib/district.rb
@@ -1,11 +1,12 @@
 class District
-  attr_reader :district_data
+  attr_reader :district_data,
+              :name
 
   def initialize(district_data)
     @district_data = district_data
   end
 
   def name
-    district_data[:name]
+    district_data[:name].upcase
   end
 end
--- lib/district_repository.rb
+++ lib/district_repository.rb
@@ -14,38 +14,21 @@ class DistrictRepository
       district_name = row[:location]#row[:location] is a string
       district_object = District.new({:name => district_name})
       district_collection[district_name] = district_object
-
-      name = row[:location]
-      district_collection[name] = District.new({:name => name}
+      #refactor for above code:
+      # name = row[:location]
+      # district_collection[name] = District.new({:name => name}
     end
-
-
   end
-
-  #need to take any CSV, pull only district name out of it, then attach that district name to instance of district class (object) and then stick that object into @district_collection
-
-  # def load_data(input)
-  #   file = input[:enrollment][:kindergarten] #calls hash from test hardcoding, which points to the value of the string accepted by CSV.open
-  #   content = CSV.open(file)
-  # end
-
-  # def load_data(file_tree)
-  #   filepath = file_tree[:enrollment][:kindergarten]
-  #   years = []
-  #   CSV.foreach(filepath, headers: true, header_converters: :symbol) do |row|
-  #     years << ({:name => row[:location], row[:timeframe].to_i => row[:data].to_f})
-  #   end
-  #   puts years
-  # end
 
   def find_by_name(district_name)
-    content = CSV.open "./data/Kindergartners in full-day program.csv", headers: true, header_converters: :symbol
-
-    content.each do |row|
-
-      return district_name.upcase if row[:location].upcase.include?(district_name.upcase)
-    end
-
+    district_collection[district_name]
   end
+
+  def find_all_matching(district_name_fragment)
+    district_collection.keys.select do |item|
+      item.include?(district_name_fragment.upcase)
+    end
+  end
+
 
 end
