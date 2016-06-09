281385937aee774f65c694d5b0c938ac13637b5d
tgisg <soniagupta504@gmail.com>
2016-06-09 12:02:26 -0600

    accessing kindergarten and load data

--- lib/district.rb
+++ lib/district.rb
@@ -1,6 +1,6 @@
 class District
-  attr_reader   :name,
-                :enrollment
+  attr_reader   :name
+  attr_accessor :enrollment
 
   def initialize(district_data, enrollment = nil)
     @name = district_data[:name].upcase
