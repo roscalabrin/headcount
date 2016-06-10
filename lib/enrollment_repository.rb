13d400b392c2eb8db93889e61d445f6ae4a8774d
Roberta Doyle <ro.scalabrin@gmail.com>
2016-06-09 20:54:31 -0600

    made enrollment repository work for more than one file

--- lib/enrollment_repository.rb
+++ lib/enrollment_repository.rb
@@ -15,22 +15,35 @@ class EnrollmentRepository
 
   def load_data(file_tree)
    filepath = file_tree[:enrollment][:kindergarten]
-   filepath_2 = file_tree[:enrollment][:high_school_graduation]
+     if file_tree[:enrollment][:high_school_graduation].nil?
+       kindergarten_array = csv_parser(filepath, :kindergarten_participation)
+       create_enrollment_object(kindergarten_array)
+     else
+       filepath_2 = file_tree[:enrollment][:high_school_graduation]
+       kindergarten_array = csv_parser(filepath, :kindergarten_participation)
+       high_school_array = csv_parser(filepath_2, :high_school_graduation)
+       merge_data(kindergarten_array, high_school_array)
+     end
 #find a way to dynamically pass in the key (i.e. :kindergarten, or high_school_graduation, or whatever)
 #maybe make a method that just extracts the filepath dynamically (def extract filepath, takes in the actual key as an arg)
-   kindergarten_array = csv_parser(filepath, :kindergarten)
-   high_school_array = csv_parser(filepath_2, :high_school_graduation)
   end
 
-  def create_enrollment_object
-    final.map do |item|
+  def merge_data(kindergarten_array, high_school_array)
+    enrollment_info = kindergarten_array.zip(high_school_array).map do |kindergarten_array|
+      kindergarten_array.reduce(&:merge)
+    end
+    create_enrollment_object(enrollment_info)
+  end
+
+  def create_enrollment_object(enrollment_info)
+    enrollment_info.map do |item|
       enrollment_object = Enrollment.new(item)
       enrollment_collection[item[:name]] = enrollment_object
+
     end
-    binding.pry
   end
 
-  def find_by_name(district_name)
+  def find_by_name(district_name)# fix this method
     enrollment_collection[district_name]
   end
 
--- test/enrollment_repository_test.rb
+++ test/enrollment_repository_test.rb
@@ -4,7 +4,7 @@ require_relative '../lib/enrollment_repository'
 class EnrollmentRepositoryTest < Minitest::Test
 
   def test_that_it_loads_data_file
-    skip
+    # skip
     er = EnrollmentRepository.new
     er.load_data({:enrollment => {:kindergarten => "./data/Kindergartners in full-day program.csv"}})
 
@@ -12,7 +12,7 @@ class EnrollmentRepositoryTest < Minitest::Test
   end
 
   def test_it_finds_instance_of_enrollment_by_name_if_exist
-    skip
+    # skip
     er = EnrollmentRepository.new
     er.load_data({:enrollment => {:kindergarten => "./data/Kindergartners in full-day program.csv"}})
     enrollment = er.find_by_name("ACADEMY 20")
@@ -21,7 +21,7 @@ class EnrollmentRepositoryTest < Minitest::Test
   end
 
   def test_it_returns_the_name_of_the_district
-    skip
+    # skip
     er = EnrollmentRepository.new
     er.load_data({:enrollment => {:kindergarten => "./data/Kindergartners in full-day program.csv"}})
     enrollment = er.find_by_name("ACADEMY 20")
@@ -30,7 +30,7 @@ class EnrollmentRepositoryTest < Minitest::Test
   end
 
   def test_that_it_loads_data_file_with_two_paths
-    
+
     er = EnrollmentRepository.new
     er.load_data({:enrollment => {:kindergarten => "./data/Kindergartners in full-day program.csv", :high_school_graduation => "./data/High school graduation rates.csv"}})
 
