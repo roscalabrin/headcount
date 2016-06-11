62ae118f4b95d6cb596b6e35ebd154aa9fc55664
tgisg <soniagupta504@gmail.com>
2016-06-11 11:25:09 -0600

    working on final part of iteration 2

--- test/headcount_analyst_test.rb
+++ test/headcount_analyst_test.rb
@@ -74,5 +74,15 @@ class HeadcountAnalystTest < Minitest::Test
     assert_equal 0.641, ha.kindergarten_participation_against_high_school_graduation('ACADEMY 20')
   end
 
+  def test_kindergarten_participation_correlates_with_high_school_graduation
+    dr = DistrictRepository.new
+    dr.load_data({:enrollment => {:kindergarten => "./data/Kindergartners in full-day program.csv", :high_school_graduation => "./data/High school graduation rates.csv"}})
+    ha = HeadcountAnalyst.new(dr)
+
+    assert ha.kindergarten_participation_correlates_with_high_school_graduation('ACADEMY 20')
+  end
+
+  #add test for refutatation of above method
+
 
 end
