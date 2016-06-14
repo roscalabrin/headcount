d3047b6c7ea85cac726c43b325f5888c6408dfd4
tgisg <soniagupta504@gmail.com>
2016-06-13 21:16:44 -0600

    I hate this project, also we started on 4

--- lib/economic_profile_repository.rb
+++ lib/economic_profile_repository.rb
@@ -1,11 +1,12 @@
 require 'pry'
 require 'csv'
 require_relative 'economic_profile'
-# require_relative 'economic_profile_parser'
+require_relative 'format'
+require_relative 'economic_profile_parser'
 
 class EconomicProfileRepository
-  # include Format
-  # include EconomicProfileParser
+  include Format
+  include EconomicProfileParser
 
   attr_reader :economic_profile_collection
 
@@ -14,21 +15,14 @@ class EconomicProfileRepository
   end
 
   def load_data(file_tree)
+    median_household_income_array = csv_parser_household(file_tree[:economic_profile][:median_household_income])
 
-    # def csv_parser(filepath, argument, key)
-    #  statewide_test_array = []
-    #  CSV.foreach(filepath, headers: true, header_converters: :symbol) do |row|
-    #    statewide_test_array <<
-    #    ({:name => row[:location].upcase, :year => row[:timeframe].to_i, row[argument].to_s.downcase => truncate(row[:data].to_f)})
-    #  end
-    #  group_data(statewide_test_array, key)
-    # end
+    children_in_poverty_array = csv_parser_children(file_tree[:economic_profile][:children_in_poverty])
 
-    median_household_income_array = csv_parser(file_tree[:economic_profile][:median_household_income], :score, 3)
-    eighth_grade_array = csv_parser(file_tree[:statewide_testing][:eighth_grade], :score, 8)
-    math_array = csv_parser(file_tree[:statewide_testing][:math], :race_ethnicity, :math)
-    reading_array = csv_parser(file_tree[:statewide_testing][:reading], :race_ethnicity, :reading)
-    writing_array = csv_parser(file_tree[:statewide_testing][:writing], :race_ethnicity, :writing)
+    free_or_reduced_price_lunch_array= csv_parser_lunch(file_tree[:economic_profile][:free_or_reduced_price_lunch], :poverty_level)
+
+    # title_i_array = csv_parser(file_tree[:economic_profile][:title_i])
+    #
   end
 
 end
